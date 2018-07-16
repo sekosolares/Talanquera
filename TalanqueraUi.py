98b5374534fbe71090433b29fb6aaec57e06654d
NoctisIbis <axel.solares.g@gmail.com>
2018-07-16 11:00:23 -0600

    Bkp Prod Version V2.2018

--- TalanqueraUi.py
+++ TalanqueraUi.py
@@ -58,7 +58,6 @@ class CallServer:
         self.__Responce = requests.post(self.__Host, params=payload)
         log.write("# [func.getEndDates]:Se ha realizado un post al server. "
                   "Respuesta del server: {0}\n".format(self.__Responce))
-
         jsonOBJ = self.__Responce.json()
 
         return jsonOBJ
@@ -70,6 +69,7 @@ WSURL = "https://diceros.ls-sys.com/Sistema/talanquera"
 nombre = 'log-'  # Nombre del archivo que lleva los logs del programa.
 fecha = time.strftime("%d-%m-%Y_%H-%M-%S")  # Fecha y hora para concatenar con el log.
 nombre += fecha
+texto_info = ''
 
 # Abriendo el archivo log para escribir en el.
 log = open(nombre+'.txt', "w")
@@ -87,17 +87,19 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
         condoaddress = "diceros.ls-sys.com"  # Se usa para verificar conexion a internet.
 
         # Setup del frame. Se fija el tamaño de la pantalla para deshabilitar el boton de maximizar.
-        QtGui.QMainWindow.setFixedWidth(self, 684)
-        QtGui.QMainWindow.setFixedHeight(self, 300)
+        QtGui.QMainWindow.setFixedWidth(self, 684)  # Ancho del form.
+        # QtGui.QMainWindow.setFixedHeight(self, 300)  # Alto del form.
+        QtGui.QMainWindow.setFixedHeight(self, 490)  # Alto del form.
 
         log.write("##### Inicio del log #####\n"
                   "# [class.TalanqueraUi]:Se ha setteado correctamente el tama{0}o de la ventana.\n".format(chr(164)))
 
         # setParent(None) para remover elementos que se crearon pero ya no se usaran.
-        self.gtxResult.setParent(None)
+        # self.gtxResult.setParent(None)
         self.lblOracleDB.setParent(None)
         self.txtODB.setParent(None)
         self.pbTestODB.setParent(None)
+        self.gtxResult.setDisabled(True)
         log.write("# [class.TalanqueraUi]:Se deshabilitan los campos, a excepción de los campos de inicio de sesión.\n")
         self.massdisable(1)
 
@@ -244,25 +246,31 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
 
     def login(self, usr, pwd):
         log.write("# [fun.login]:Botón de Login presionado. Llamada a función correcta...\n")
+        self.gtxResult.setText("")
+        self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Enviando datos al servidor...\n")
         with CallServer(WSURL) as ws:
+            self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Esperando respuesta del servidor...\n")
             response = ws.LogIn(usr.upper(), pwd)
             log.write("# [func.login]:Se evalua el json recibido: {0}\n".format(response))
+            self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "El servidor ha respondido...\n")
+            self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Evaluando respuesta del servidor...\n")
             if response['ACK'] == '1':
                 log.write("# [func.login]:Valores correctos. Inicio de Sesión exitoso...\n")
+                self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Correcto... Ingreso exitoso.\n")
                 self.alert("Control Condominio", "¡Usuario y Contraseña VÁLIDOS!")
                 log.write("# [func.login]:Procede a deshabilitar sección de Inicio de Sesión.\n")
                 self.massdisable(2)
                 # self.txtADB.setText(response['PDB'])
             else:
                 log.write("# [func.login]:Valores incorrectos. No Inicia Sesión...\n")
+                self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Incorrecto... Ingreso fallido.\n")
                 self.alert("Control Condominio", "¡Usuario y Contraseña NO VÁLIDOS!")
 
     def actualizar(self, dbruote, modo):
         log.write("# [func.actualizar]:Llamada a función correcta. "
                   "Params: dbroute={0} ; modo={1}\n".format(dbruote, modo))
+        self.gtxResult.setText("")
         try:
-
-            SQLsUpdate = []
 
             with CallServer(WSURL) as ws:
                 strUsr = str(self.txtUsuario.text())
@@ -272,22 +280,45 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
                 log.write("# [func.actualizar]:Respuesta recibida del server: {0}\n".format(response))
                 counterI = 0
                 counterU = 0
-                # in_clause = ""
+                ubicador = 0
 
-                log.write("# [func.actualizar]:Inicio de bucle...\n")
+            if modo == 'odbc':
+                # Conexion con odbc.
+
+                db = dbruote
+                constr = ('Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=' + db)
+                log.write("# [func.actualizar]:Conexión a Access. String de conexión: {0}\n".format(constr))
+                conn = pyodbc.connect(str(constr), autocommit=True)
+                log.write("# [func.actualizar]:Conexión: conn={0}\n".format(conn))
+
+                cur = conn.cursor()
+                log.write("# [func.actualizar]:Conexión: cur={0}\n".format(cur))
+                self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Iniciando interaccion"
+                                                                           " en base de datos...\n")
+                log.write("# [func.actualizar]:Inicio de bucle que ejecuta los updates/inserts...\n")
+
+                QtGui.QApplication.processEvents()
                 for codTarjeta, bloque in response.iteritems():
                     tipo = bloque[1]
-                    log.write("# [func.actualizar]:Tipo a evaluar. 2=existe ; 1=nuevo. "
+                    log.write("# [func.actualizar]:Tipo a evaluar. 2=existe ; 1=nuevo... "
                               "tipo={0}\n".format(tipo))
+
                     if tipo == '2':
                         sql = "UPDATE {0} SET {1} = Format('{2}', 'yyyy-mm-dd') " \
                               "where Mid( Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1)," \
                               "InStr(1, Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1) , '-')+1) = '{4}'" \
                               .format("TEmployee", "EndDate", bloque[0][0:10], "EmployeeCode", codTarjeta)
                         log.write("# [func.actualizar]:Realiza update en Access. sql= {0}\n".format(sql))
-                        SQLsUpdate.append(sql)
+                        cur.execute(sql)
                         counterU += 1
-                        # in_clause += "'" + codTarjeta + "', "
+                        ubicador += 1
+                        self.gtxResult.setText(str(self.gtxResult.toPlainText())
+                                               + "Actualizado... {0}\n".format(counterU))
+                        log.write("# [func.actualizar]:Update en Access realizado. Numero {0}\n".format(counterU))
+                        for x in range(ubicador):
+                            self.gtxResult.moveCursor(QtGui.QTextCursor.Down, QtGui.QTextCursor.MoveAnchor)
+                        if ubicador % 4 == 1:
+                            QtGui.QApplication.processEvents()
                     elif tipo == '1':
                         sql = "Insert into {0}" \
                               "(EmployeeID, EmployeeCode, EmployeeName, CardNo, pin, EmpEnable, Birthday, " \
@@ -297,48 +328,18 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
                               .format("TEmployee", bloque[2], bloque[3].upper(), codTarjeta, 1234, 0, "Date()",
                                       "Date()", bloque[0][0:10], 0, 0, 0, 1234)
                         log.write("# [func.actualizar]:Realiza insert en Access. sql=  {0}\n".format(sql))
-                        SQLsUpdate.append(sql)
+                        cur.execute(sql)
                         counterI += 1
-                # log.write("# [func.actualizar]:Se construye un 'in clause' "
-                #          "para determinar cantidad real de updates. Valor del in: {0}\n".format(in_clause))
-                log.write("# [func.actualizar]:El listado de sqls a ejecutar:  {0}\n".format(SQLsUpdate))
+                        ubicador += 1
+                        self.gtxResult.setText(str(self.gtxResult.toPlainText())
+                                               + "Ingresado... {0}\n".format(counterI))
+                        log.write("# [func.actualizar]:Insert en Access realizado. Numero:: {0}\n".format(counterI))
+                        for x in range(ubicador):
+                            self.gtxResult.moveCursor(QtGui.QTextCursor.Down, QtGui.QTextCursor.MoveAnchor)
+                        if ubicador % 4 == 1:
+                            QtGui.QApplication.processEvents()
+
                 log.write("\n# [func.actualizar]:Fin de bucle...\n")
-            if modo == 'odbc':
-                """
-                connects with odbc
-                """
-                db = dbruote
-                constr = ('Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=' + db)
-                log.write("# [func.actualizar]:Conexión a Access. String de conexión: {0}\n".format(constr))
-                conn = pyodbc.connect(str(constr), autocommit=True)
-                log.write("# [func.actualizar]:Conexión: conn={0}\n".format(conn))
-
-                cur = conn.cursor()
-                log.write("# [func.actualizar]:Conexión: cur={0}\n".format(cur))
-
-                # fin = len(in_clause)
-                # fin -= 2
-                # in_clause = in_clause[0:fin]
-
-                #verifier = "select enddate from TEmployee " \
-                #           "where Mid( Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1)," \
-                #           "InStr(1, Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1) , '-')+1) in ({0})" \
-                #           .format(in_clause)
-
-                # log.write("# [func.actualizar]:Sql para verificar cantidad más exacta "
-                #          "de updates: verifier={0}\n".format(verifier))
-
-                log.write("# [func.actualizar]:Se ejecutaran los sql enlistados. Sentencias:\n")
-                for sentencia in SQLsUpdate:
-                    cur.execute(sentencia)
-                    log.write("# [func.actualizar]:{0}\n".format(sentencia))
-
-                log.write("# [func.actualizar]:Final de sentencias enlistadas...\n")
-                # cur.execute(verifier)
-                # log.write("# [func.actualizar]:Se ha ejecutado el sql para contar los updates...\n")
-                # t = tuple(cur)
-                # log.write("# [func.actualizar]:Resultado del sql ejecutado:  {0}\n".format(t))
-                # counterU = len(t)
 
                 conn.close()
                 log.write("# [func.actualizar]:Conexión a Access cerrada...\n")
@@ -357,9 +358,7 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
                 log.close()
                 sys.exit(0)
             elif modo == 'ado':
-                """
-                connect with com dispatch objs
-                """
+                # Conexion con ado
                 db = dbruote
                 conn = win32com.client.Dispatch(r'ADODB.Connection')
                 log.write("# [func.actualizar]:Conexión a Access en proceso... conn={0}\n".format(conn))
@@ -369,28 +368,53 @@ class TalanqueraUi(QtGui.QMainWindow, Ui_MainWindow):
 
                 rs = win32com.client.Dispatch(r'ADODB.Recordset')
                 log.write("# [func.actualizar]:Conectado a Access... rs={0}\n".format(rs))
-                # fin = len(in_clause)
-                # fin -= 2
-                # in_clause = in_clause[0:fin]
+                self.gtxResult.setText(str(self.gtxResult.toPlainText()) + "Iniciando interaccion "
+                                                                           "en base de datos...\n")
+                log.write("# [func.actualizar]:Inicio de bucle que ejecuta los updates/inserts...\n")
 
-                # verifier = "select enddate from TEmployee " \
-                #           "where Mid( Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1)," \
-                #           "InStr(1, Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1) , '-')+1) in ({0})" \
-                #           .format(in_clause)
+                QtGui.QApplication.processEvents()
+                for codTarjeta, bloque in response.iteritems():
+                    tipo = bloque[1]
+                    log.write("# [func.actualizar]:Tipo a evaluar. 2=existe ; 1=nuevo... "
+                              "tipo={0}\n".format(tipo))
 
-                #log.write("# [func.actualizar]:Sql para verificar cantidad más exacta "
-                #          "de updates: verifier={0}\n".format(verifier))
+                    if tipo == '2':
+                        sql = "UPDATE {0} SET {1} = Format('{2}', 'yyyy-mm-dd') " \
+                              "where Mid( Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1)," \
+                              "InStr(1, Mid([EmployeeCode], InStr(1, [EmployeeCode], '-')+1) , '-')+1) = '{4}'" \
+                              .format("TEmployee", "EndDate", bloque[0][0:10], "EmployeeCode", codTarjeta)
+                        log.write("# [func.actualizar]:Realiza update en Access. sql= {0}\n".format(sql))
+                        rs.Open(sql, conn, 1, 3)
+                        counterU += 1
+                        ubicador += 1
+                        log.write("# [func.actualizar]:Update en Access realizado. Numero:: {0}\n".format(counterU))
+                        self.gtxResult.setText(str(self.gtxResult.toPlainText())
+                                               + "Actualizado... {0}\n".format(counterU))
+                        for x in range(ubicador):
+                            self.gtxResult.moveCursor(QtGui.QTextCursor.Down, QtGui.QTextCursor.MoveAnchor)
+                        if ubicador % 4 == 1:
+                            QtGui.QApplication.processEvents()
+                    elif tipo == '1':
+                        sql = "Insert into {0}" \
+                              "(EmployeeID, EmployeeCode, EmployeeName, CardNo, pin, EmpEnable, Birthday, " \
+                              "RegDate, EndDate, ACCESSID, Deleted, Leave, Password)" \
+                              "values('{1}', '{1}', '{2}', '{3}', {4}, {5}, {6}, {7}, " \
+                              "Format('{8}', 'yyyy-mm-dd'), {9}, {10}, {11}, '{12}')" \
+                              .format("TEmployee", bloque[2], bloque[3].upper(), codTarjeta, 1234, 0, "Date()",
+                                      "Date()", bloque[0][0:10], 0, 0, 0, 1234)
+                        log.write("# [func.actualizar]:Realiza insert en Access. sql=  {0}\n".format(sql))
+                        rs.Open(sql, conn, 1, 3)
+                        counterI += 1
+                        ubicador += 1
+                        self.gtxResult.setText(str(self.gtxResult.toPlainText())
+                                               + "Ingresado... {0}\n".format(counterI))
+                        log.write("# [func.actualizar]:Insert en Access realizado. Numero:: {0}\n".format(counterI))
+                        for x in range(ubicador):
+                            self.gtxResult.moveCursor(QtGui.QTextCursor.Down, QtGui.QTextCursor.MoveAnchor)
+                        if ubicador % 4 == 1:
+                            QtGui.QApplication.processEvents()
 
-                log.write("# [func.actualizar]:Se ejecutaran los sql enlistados. Sentencias:\n")
-                for sentencia in SQLsUpdate:
-                    rs.Open(sentencia, conn, 1, 3)
-                    log.write("# [func.actualizar]:{0}\n".format(sentencia))
-
-                # t = rs.Open(verifier, conn, 1, 3)
-                # t = tuple(t)
-                # log.write("# [func.actualizar]:Se ha ejecutado el sql para contar los updates...\n")
-                # log.write("# [func.actualizar]:Resultado del sql ejecutado:  {0}\n".format(t))
-                # counterU = len(t)
+                log.write("\n# [func.actualizar]:Fin de bucle...\n")
 
                 conn.Close()
                 log.write("# [func.actualizar]:Conexión a Access cerrada...\n")
